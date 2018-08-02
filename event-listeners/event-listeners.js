a4812878cd0c8b1950c97f1847dd40935984ca0b
aboud20-meet <aboud20@meet.mit.edu>
2018-08-02 16:42:09 +0300

    fun!

--- event-listeners/event-listeners.js
+++ event-listeners/event-listeners.js
@@ -1,5 +1,5 @@
 // YOUR TASK: Add more pictures!
-var pictures = ['./imgs/dog.jpg'];
+var pictures = ['./imgs/dog.jpg','./imgs/77e3ddf868d6156029d47c78b609c47e--zodiac-signs-tumblr-productivity-quotes.jpg','./imgs/68401382-tumblr-wallpapers.jpg','./imgs/Iphone-8-Wallpaper-Tumblr-Hipster.jpg','./imgs/wallpaper2you_583589.jpg'];
 var currentIndex = 0;
 
 function showNextPicture() {
@@ -8,6 +8,7 @@ function showNextPicture() {
   if (currentIndex >= pictures.length) {
     currentIndex = 0;
   }
-
-  // YOUR TASK: Finish this function!
+document.getElementsByTagName("img")[0].src=pictures[currentIndex];
 }
+
+document.getElementsByTagName("img")[0].addEventListener("click",showNextPicture);
